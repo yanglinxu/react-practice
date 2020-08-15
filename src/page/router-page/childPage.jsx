ae1a60b65af1afd7c8aa5ef4b2731d58f2091872
ylx <28937402@qq.com>
2020-08-15 16:46:55 +0800

    update

--- src/page/router-page/childPage.jsx
+++ src/page/router-page/childPage.jsx
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
 // 如果组件没有自己的状态或者要调用的方法  不需要显式声明constructor
 export default class childPage extends Component {
   render() {
-    console.log(this.props.name);
+    console.log(this.props);
     return (
       <div>
         我是嵌套路由的组件呦～
--- src/page/router-page/index.js
+++ src/page/router-page/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { HashRouter, Route, Switch, Link, Redirect } from 'react-router-dom';
-import childPage from "./childPage";
+import ChildPage from "./childPage";
 export default class index extends Component {
   render() {
     return (
@@ -9,7 +9,8 @@ export default class index extends Component {
             所以通常路由规则长的放在上面  短的放在下面
         */}
         <Switch>
-          <Route path='/indexPage' component={Home}></Route>
+          <Route path='/indexPage' component={Home}>
+          </Route>
           <Route path='/' component={Index}></Route>
         </Switch>
 
@@ -22,10 +23,15 @@ function Home({ match }) {
   return (
     <>
       <h1>我是home路由组件</h1>
-      <Route path={match.url + "/child"} name="123" component={childPage}></Route>
+      {/* 当通过component渲染嵌套路由时 字路由只能接收路由参数 无法通过props方式传值*/}
+      <Route path={match.url + "/child"} component={ChildPage}></Route>
+      {/* 通过render渲染时 可以通过props指定传入的参数 */}
+      <Route path={match.url + "/child"} render={() =>
+        <ChildPage test='123'></ChildPage>
+      }></Route>
       {/* 跳转路由 */}
       <Link to="/">跳转标签</Link>
-      <Index text={match}></Index>
+      {/* <Index text={match}></Index> */}
       {/* Redirect一旦渲染  页面强制执行跳转 */}
       {/* <Redirect to="/"></Redirect> */}
     </>
